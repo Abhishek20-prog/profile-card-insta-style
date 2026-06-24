01819c737982395492c9b0dce73b2a5c697ec50a
Abhishek20-prog <Abhishekkumastudent1234@gmail.com>
2026-06-24 19:28:14 +0530

    initial commit

--- instacard/src/components/Card.jsx
+++ instacard/src/components/Card.jsx
@@ -4,7 +4,11 @@ const Card = () => {
   return (
     <div className="parent">
       <div className="box">
-        <div className="logo"></div>
+        <div className="header">
+          <div className="logo"></div>
+        <div className="save">save</div>
+        </div>
+        <div className="name"></div>
       </div>
     </div>
   )
