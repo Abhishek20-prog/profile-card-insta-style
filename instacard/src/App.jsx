9c8503da9a7dc820e76cadf91697edce6861725b
Abhishek20-prog <Abhishekkumastudent1234@gmail.com>
2026-06-25 20:48:02 +0530

    initial commit

--- instacard/src/App.jsx
+++ instacard/src/App.jsx
@@ -2,8 +2,9 @@ import Card from './components/Card.jsx'
 
 const App = () => {
   return (
-    <div>
-      <Card/>
+    <div className="parent">
+      <Card name="Amazon" days="5 days ago" position="Senior UI/UX designer" timing="Part-time" others="Senior Role" salary="$120/hr" location="Mumbai,India" img="https://images.icon-icons.com/1195/PNG/512/1490889698-amazon_82521.png"/>
+      <Card name="Google" days="6 days ago" position="Senior Developer" timing="Part-time" others="Senior Role" salary="$200/hr" location="Bengaluru,India" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6U4pRN-hmnCKRp5bBpsxjHfbbutWDEgFRUo0YrTP2ag&s=10"/>
     </div>
   )
 }
--- instacard/src/components/Card.jsx
+++ instacard/src/components/Card.jsx
@@ -1,32 +1,32 @@
 
 
 
-const Card = () => {
+const Card = (prop) => {
   return (
     <div className="parent">
       <div className="box">
         <div className="header">
-          <div className="logo"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSF_BFXkdVHGZNGMQAwulii6n-me8ok0UTxLQYKkdIhoQ&s=10" alt="" srcset="" /></div>
+          <div className="logo"><img src={prop.img} alt="" srcset="" /></div>
         <button className="save-btn">
   Save
   <span className="save-icon">♡</span>
 </button>
         </div>
-        <div className="details"><div className="name">Amazon
-          <div className="days">5 Days ago</div>
+        <div className="details"><div className="name">{prop.name}
+          <div className="days">{prop.days}</div>
           
         </div>
-        <div className="position">Senior UI/UX Designer</div>
+        <div className="position">{prop.position}</div>
         <div className="dets">
-          <div className="timing">Part-time</div>
-          <div className="others">Senior Level</div>
+          <div className="timing">{prop.timing}</div>
+          <div className="others">{prop.others}</div>
 
 
         </div>
         </div>
         <div className="line"></div>
         <div className="footer">
-          <div className="salary">$120/hr <div className="location">Mumbai, India</div></div>
+          <div className="salary">{prop.salary} <div className="location">{prop.location}</div></div>
           <button className="btn">Apply Now</button>
         </div>
         
